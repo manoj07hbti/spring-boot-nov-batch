2fda66e532f5bfe9081a89460b0f495b8c1b4401
sandeep20198 <sandeepsingh20198@gmail.com>
2023-01-03 13:30:01 +0530

    Spring boot (odd_even)

---
+++ src/main/java/com/example/springbootnovbatch/odd_even/Odd_EVEN.java
@@ -0,0 +1,43 @@
+package com.example.springbootnovbatch.odd_even;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Odd_EVEN {
+
+    //odd or even for single digit
+
+    @RequestMapping("/oddeven")
+    public void odd() {
+
+        int a = 10 ;
+        if (a%2==0){
+            System.out.println("EVEN "+a);
+        }else {
+            System.out.println("ODD "+a);
+        }
+
+        // http://localhost:8081/oddeven
+    }
+
+
+    // method for 1 to 100
+    @RequestMapping("/evenodd")
+    public void oddeven (){
+
+        for (int i=1; i<=100; i++){
+            if (i%2==0){
+                System.out.println("Even "+i);
+            }else{
+                System.out.println("odd "+i);
+            }
+        }
+    }
+    // http://localhost:8081/evenodd
+}
+
+
+
+
+
