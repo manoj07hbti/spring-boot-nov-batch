c1697ee3cdebba13deee165ee65e8b9e88fea1a0
aakas <iamakashg360@gmail.com>
2023-01-03 10:47:01 +0530

    Calculator Controller all logic

--- src/main/java/com/example/springbootnovbatch/controller/CalculatorController.java
+++ src/main/java/com/example/springbootnovbatch/controller/CalculatorController.java
@@ -16,4 +16,26 @@ public class CalculatorController {
         return "Addition is :"+sum;
 
     }
+    @RequestMapping("/sub")
+    public String sub (){
+        int a= 15, b=5;
+        int sub = a-b;
+
+        return "Subtraction of "+a+" - "+b+" = "+sub;
+    }
+
+    @RequestMapping("/multiply")
+    public String multiply (){
+        int a= 10, b=10;
+        int multi = a*b;
+
+        return "Multiplication of "+a+" * "+b+" = "+multi;
+    }
+    @RequestMapping("/divide")
+    public String divide (){
+        double a= 15, b=5;
+        double div = a/b;
+
+        return "Divide of "+a+" / "+b+" = "+div;
+    }
 }
