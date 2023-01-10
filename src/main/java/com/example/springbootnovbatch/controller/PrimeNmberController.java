4b48305e8f1ecac4399e0e10199f48e85391631f
feature-varun <pratapvarun21@gmail.com>
2023-01-11 04:18:45 +0530

    path-variable-prime-number

---
+++ src/main/java/com/example/springbootnovbatch/controller/PrimeNmberController.java
@@ -0,0 +1,27 @@
+package com.example.springbootnovbatch.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeNmberController {
+
+    @RequestMapping("/check_prime/{num}")
+    public String primenumber(@PathVariable int num){
+        int check = 0; String output;
+        for(int i=2; i<num; i++){
+            if (num%i==0){
+                check+=1;
+            }
+        }
+        if (check>0){
+            output = num+" is not a Prime Number";
+        }else {
+            output = num+" is a Prime Number";
+        }
+        return output;
+    }
+
+}
