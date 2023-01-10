e6ee98f63b921987ca3ab26f3373f6afe30b08de
aakas <iamakashg360@gmail.com>
2023-01-10 16:49:38 +0530

    Employee CRUD Operation Assignment

---
+++ src/main/java/com/example/springbootnovbatch/controller/EmployeeCrudController.java
@@ -0,0 +1,60 @@
+package com.example.springbootnovbatch.controller;
+
+import com.example.springbootnovbatch.model.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class EmployeeCrudController {
+
+    ArrayList<Employee> employeeData = new ArrayList<>();
+
+    //Create section
+    @RequestMapping("/addemployee/{name}/{age}/{department}")
+    public String addemployee(@PathVariable String name,@PathVariable int age,@PathVariable String department){
+        Employee employee = new Employee(name,age,department);
+        employeeData.add(employee);
+
+        return "Employee data added successfully";
+
+    }
+    //Read Section
+
+    @RequestMapping("/getemployee")
+    public ArrayList<Employee> getEmployeeData() {
+        return employeeData;
+    }
+
+    //Update name Section
+    @RequestMapping("/updateemployee_name/{name}/{index}")
+    public String updateemployeename(@PathVariable String name,@PathVariable int index){
+        Employee employee = employeeData.get(index);
+        employee.setName(name);
+        return "Employee name updated successfully";
+    }
+    //Update Age Section
+    @RequestMapping("/updateemployee_age/{age}/{index}")
+    public String updateemployeeage(@PathVariable int age,@PathVariable int index){
+        Employee employee = employeeData.get(index);
+        employee.setAge(age);
+        return "Employee age updated successfully";
+    }
+    //Update department Section
+    @RequestMapping("/updateemployee_department/{department}/{index}")
+    public String updateemployeedepartment(@PathVariable String department,@PathVariable int index){
+        Employee employee = employeeData.get(index);
+        employee.setDepartment(department);
+        return "Employee department updated successfully";
+    }
+
+    // delete section
+    @RequestMapping("/removeemployee")
+    public String removeemployee(@RequestParam int index){
+        employeeData.remove(index);
+        return "Employee data removed successfully";
+    }
+}
---
+++ src/main/java/com/example/springbootnovbatch/model/Employee.java
@@ -0,0 +1,42 @@
+package com.example.springbootnovbatch.model;
+
+public class Employee {
+
+    String name;
+    int age;
+    String department;
+
+    // parameterized constructor
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    // Getter and Setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
